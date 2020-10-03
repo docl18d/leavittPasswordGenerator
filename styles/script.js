69906b71cc94acce1d64160860fcca7faf72c7f4
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2020-10-02 19:45:59 -0600

    forgot to hit save before push

---
+++ styles/script.js
@@ -0,0 +1,24 @@
+// characterset assignment
+let randomCharString = "";
+let charSet = [
+    {
+        char: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+        name: "Special Characters",
+        use: false
+    },
+    {
+        char: "0123456789",
+        name: "Number Characters",
+        use: false
+    },
+    {
+        char: "abcdefghijklmnopqrstuvwxyz",
+        name: "Lowercase Letters",
+        use: false
+    },
+    {
+        char: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        name: "Uppercase Letters",
+        use: false
+    }
+]
